3c42dbb5969980e07c4a5a074148ffc2b1e51cb9
Nundini <nundinir@umich.edu>
2024-08-19 23:45:07 -0400

    added main script

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+def generate_random_number():
+    return random.randint(1, 10)
+
+def check_even_odd(number):
+    if number % 2 == 0:
+        print("The number is odd.")
+    else:
+        print("The number is even.")
+
+def main():
+    number = generate_random_number()
+    print("Generated number:", number)
+    check_even_odd(number)
+
+if __name__ == "__main__":
+    main()
